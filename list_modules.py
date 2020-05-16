2f610e583902c839261886ddb3a882c28386eac1
Strahinja Lukić <strahinja.lukic.011@gmail.com>
2020-05-16 18:56:42 +0200

    [DEV] Parse module strings into dicts (part I)

--- list_modules.py
+++ list_modules.py
@@ -21,11 +21,27 @@ def split_module_strings(module_list_string):
 
 
 class PAModule:
+    def __init__(self):
+        self.attributes = {}
+        self.properties = {}
+        self.lines = []
+
+    @staticmethod
+    def parse_value(value_str):
+        return True if value_str.lower() == 'yes' else False if value_str.lower() == 'no' else value_str
+
     @classmethod
     def parse_module_string(cls, module_string):
         pa_module = cls()
-        name_match = re.search(r'^\s+name:.*<(.+)>', module_string)
-        pa_module.name = name_match.group(1)
+        pa_module.lines = module_string.splitlines()
+        for line in pa_module.lines:
+            arg_match = re.search(r'^\s+(.+):\s+(.*)', line)
+            if arg_match:
+                assert(len(arg_match.groups()) == 2)
+                attr_key = arg_match.group(1)
+                attr_valstr = arg_match.group(2)
+                attr_value = cls.parse_value(attr_valstr)
+                pa_module.attributes[attr_key] = attr_value
         return pa_module
 
 
@@ -35,4 +51,4 @@ if __name__ == "__main__":
 
     modules = [PAModule.parse_module_string(modstr) for modstr in mods]
     for module in modules:
-        print('Module name: ' + module.name)
+        print(module.attributes)
