479909536992e3fa2cf44917f486ba9c780a612b
Strahinja Lukić <strahinja.lukic.011@gmail.com>
2020-05-16 22:42:01 +0200

    [DEV] Add parsing of properties

--- list_modules.py
+++ list_modules.py
@@ -21,6 +21,9 @@ def split_module_strings(module_list_string):
 
 
 class PAModule:
+    re_attr = re.compile(r'^\s+(.+):\s*(.*)')
+    re_prop = re.compile(r'^\s+(.+)\s+=\s+"(.*)"')
+
     def __init__(self):
         self.attributes = {}
         self.properties = {}
@@ -31,17 +34,34 @@ class PAModule:
         return True if value_str.lower() == 'yes' else False if value_str.lower() == 'no' else value_str
 
     @classmethod
+    def add_item(cls, arg_match, dictionary):
+        attr_key = arg_match.group(1)
+        attr_value = cls.parse_value(arg_match.group(2))
+        dictionary[attr_key] = attr_value
+
+    @classmethod
     def parse_module_string(cls, module_string):
         pa_module = cls()
         pa_module.lines = module_string.splitlines()
+
+        active = {'regex': cls.re_attr, 'dict': pa_module.attributes}
+
         for line in pa_module.lines:
-            arg_match = re.search(r'^\s+(.+):\s+(.*)', line)
+            arg_match = active['regex'].search(line)
             if arg_match:
                 assert(len(arg_match.groups()) == 2)
                 attr_key = arg_match.group(1)
-                attr_valstr = arg_match.group(2)
-                attr_value = cls.parse_value(attr_valstr)
-                pa_module.attributes[attr_key] = attr_value
+                if attr_key == 'properties':
+                    active['regex'] = cls.re_prop
+                    active['dict'] = pa_module.properties
+                    continue
+                cls.add_item(arg_match, active['dict'])
+            else:
+                active['regex'] = cls.re_attr
+                active['dict'] = pa_module.attributes
+                arg_match = active['regex'].search(line)
+                if arg_match:
+                    cls.add_item(arg_match, active['dict'])
         return pa_module
 
 
@@ -52,3 +72,4 @@ if __name__ == "__main__":
     modules = [PAModule.parse_module_string(modstr) for modstr in mods]
     for module in modules:
         print(module.attributes)
+        print(module.properties)
