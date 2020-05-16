8cac4309b486d423f7b97d52e047151fc8033219
Strahinja Lukić <strahinja.lukic.011@gmail.com>
2020-05-16 16:29:50 +0200

    [DEV] Add get_module_strings function

--- list_modules.py
+++ list_modules.py
@@ -1,12 +1,20 @@
 import commands
 import subprocess
+import re
 
 
-def module_list_str():
+def get_module_list():
     result = subprocess.run(commands.list_modules, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     print('Captured the list')
     return result.stdout.decode("utf-8")
 
 
+def get_module_strings(module_list_string):
+    module_strings = re.split(r'index: ([\d])', module_list_string)
+    return module_strings
+
+
 if __name__ == "__main__":
-    print(module_list_str())
+    mods = get_module_strings(get_module_list())
+    print(mods[1])
+    print(mods[2])
